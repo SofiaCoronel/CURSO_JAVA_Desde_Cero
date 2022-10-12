aaf999b0cb66d5bf47fc750b910b5689ca71894c
SofiaCoronel <sofia.coronel.2001@gmail.com>
2022-10-12 16:29:04 -0300

    ultimos agregados

--- ClasePracticaPrimerPrcialLab2/ComortamientoPersonas.java
+++
@@ -1,9 +0,0 @@
-package ClasePracticaPrimerPrcialLab2;
-
-public interface ComortamientoPersonas {
-    public abstract String movimientoPersona();
-    public abstract void lenguajePersona();
-    public static final double salarioBase=1200;
-    private static final double incentivo=300;
-}
-
