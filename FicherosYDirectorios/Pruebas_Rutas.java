1a5a89d8c1375f879b25f3ada2ca7c03f19621f9
SofiaCoronel <sofia.coronel.2001@gmail.com>
2022-10-22 18:33:15 -0300

    Imprimimos lo que tiene una Carpeta

--- FicherosYDirectorios/Pruebas_Rutas.java
+++ FicherosYDirectorios/Pruebas_Rutas.java
@@ -1,2 +1,18 @@
-package FicherosYDirectorios;public class Pruebas_rutas {
+package FicherosYDirectorios;
+import java.io.*;
+
+public class Pruebas_Rutas {
+    public static void main(String[] args) {
+
+        File archivo = new File("ejemplo_archivo");   // toma como ruta por defecto, la carpeta del proyecto
+        System.out.println(archivo.getAbsolutePath());  // imprime la ruta del archivo
+        System.out.println(archivo.exists());   // ver si el archivo existe (BOOLEAN)
+
+
+        File acrhivo2 = new File("src");    // colocamos una carpeta que si existe
+        System.out.println(acrhivo2.getAbsolutePath());
+        System.out.println(acrhivo2.exists());
+
+
+    }
 }
