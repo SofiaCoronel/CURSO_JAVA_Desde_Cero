5d71bc4b751ebbd21654128989e5253e38438e32
SofiaCoronel <sofia.coronel.2001@gmail.com>
2022-10-26 23:28:30 -0300

    GUI

--- Graficos/GUI/Pantalla.java
+++ Graficos/GUI/Pantalla.java
@@ -1,2 +1,26 @@
-package Graficos.GUI;public class Pantalla {
+package Graficos.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Pantalla {
+    public static void main(String[] args) {
+
+                Ventana marco = new Ventana();
+                marco.setSize(500,300);
+                marco.setVisible(true);
+                marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
+class Ventana extends JFrame{
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton verNombreButton;
+    private JButton guardarButton;
+
+    public Ventana(){
+
+        add(textField1, BorderLayout.CENTER);
+    }
 }
--- Graficos/GUI/Ventana.java
+++ Graficos/GUI/Ventana.java
@@ -1,2 +1,17 @@
-package Graficos.GUI;public class Ventana {
+package Graficos.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+/*
+public class Ventana extends JFrame{
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton verNombreButton;
+    private JButton guardarButton;
+
+    public Ventana(){
+        add(textField1, BorderLayout.CENTER);
+    }
 }
+
+ */
